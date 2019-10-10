ee5bf799e7a776ad8f17d0715456d6bc38490e72
zhengjiawen <zhengjiawen77@gmail.com>
2019-10-10 13:59:05 +0800

    add rotate

--- img_noisy.py
+++ img_noisy.py
@@ -1,6 +1,9 @@
 import cv2 as cv
 import imgaug as ia
+from imgaug import augmenters as iaa
 import os
+
+ia.seed(4)
 
 base_path = '/data/home/zjw/dataset/face_test2w/'
 test_img_path = 'test_sample/'
@@ -11,7 +14,13 @@ output_path = 'aug_test/output/'
 
 img = cv.imread(os.path.join(base_path, test_img_path, test_img), 1)
 
-cv.imwrite(os.path.join(base_path, output_path, 'result1.jpg'), img)
+# img aug
+rotate = iaa.Affine(rotate=(-25, 25))
+image_aug = rotate.augment_image(img)
+
+img_output = os.path.join(base_path, output_path, 'result1.jpg')
+print(img_output)
+cv.imwrite(img_output, image_aug)
 
 
 
